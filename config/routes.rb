0458a65647fc194959b3d89db9e03144c58b3965
KENSUKE NAKANISHI <kensukenakanishi@Ken.local>
2017-02-18 20:55:09 +0900

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/show'
+
   root 'videos#index'
   resources :curation_likes
   resources :curation_comments
@@ -9,7 +11,9 @@ Rails.application.routes.draw do
   resources :video_likes
   resources :video_comments
   resources :videos
+  get 'users/:id', to: 'users#show', as: :user
   devise_for :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
