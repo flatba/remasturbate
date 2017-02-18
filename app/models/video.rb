8e90bcb5a899ce63bd00a7738b1972380e161543
Yoshiki Hiraba <flatba@YsMac.local>
2017-02-18 21:58:51 +0900

    ブランチを切る前に一旦コミット

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :user
   has_many :video_tag
+  has_many :videos
   has_many :tags, :through => :video_tag
   has_many :video_comments
   has_many :users, :through => :video_comments
