4a4221add4eeb687b62773c1ea0b39f05649afce
KENSUKE NAKANISHI <kensukenakanishi@Ken.local>
2017-02-18 19:06:35 +0900

    ひとまずメールアドレスは必須にしてすすめる

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,6 @@ Rails.application.routes.draw do
   resources :video_comments
   resources :videos
   devise_for :users
-    
-    # get 'pages/index'
-    # get 'pages/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
