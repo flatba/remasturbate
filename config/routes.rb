c2b1711a5ed596c1432276152f26f6b103cbb2b6
Yoshiki Hiraba <flatba@YsMac.local>
2017-02-18 20:27:26 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-    devise_for :users
-    root 'videos#index'
-    get 'pages/show'
-    resources :curation_likes
-    resources :curation_comments
-    resources :curation_videos
-    resources :curations
-    resources :video_tags
-    resources :tags
-    resources :video_likes
-    resources :video_comments
-    resources :videos
-
-    # get 'pages/index'
-    # get 'pages/show'
-=======
   root 'videos#index'
   resources :curation_likes
   resources :curation_comments
@@ -27,7 +10,5 @@ Rails.application.routes.draw do
   resources :video_comments
   resources :videos
   devise_for :users
->>>>>>> fd92722648e32277da304b893747d76e076bbc79
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20170215224233_devise_create_users.rb
+++ db/migrate/20170215224233_devise_create_users.rb
@@ -3,12 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: true, default: ""
-<<<<<<< HEAD
-      t.string :encrypted_password, null: true, default: ""
-=======
       t.string :encrypted_password, null: false, default: ""
->>>>>>> fd92722648e32277da304b893747d76e076bbc79
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
