ea0e29911e79075bcb95c4844042a44849e49eb7
Yoshiki Hiraba <flatba@YsMac.local>
2017-02-18 20:21:21 +0900

    videoのviewのindexのサムネイルにlink_toを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
     root 'videos#index'
     get 'pages/show'
     resources :curation_likes
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     resources :video_likes
     resources :video_comments
     resources :videos
-    
+
     # get 'pages/index'
     # get 'pages/show'
 
--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20170216115554) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: ""
+    t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
